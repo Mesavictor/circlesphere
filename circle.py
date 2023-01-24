7013d3e34c8d5a8535eda0d8cada204ef240643c
Mesavictor <vimelblues@gmail.com>
2023-01-24 17:49:41 +0300

    frst commit

---
+++ circle.py
@@ -0,0 +1,5 @@
+#program to calculate the area of a circle
+pi=3.142
+radius=int(input("Enter the radius of the circle"))
+area=pi*radius*radius
+print("The area of the circle=",area)
---
+++ sphere.py
@@ -0,0 +1,5 @@
+#program to calculate the volume of a sphere
+pi=3.142
+radius=int(input("Enter the radius of the sphere: "))
+volume=1.333*pi*radius*radius*radius
+print("The volume of the sphere =",volume)
